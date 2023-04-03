4a454626dd8a0cd35df59e6d04e8c082cef4c5db
Cheesy <amgharmahdi1@gmail.com>
2023-04-03 16:56:09 +0000

    0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints an non playable chessboard
+ * @a: the board to print
+ * Return: Always 0.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int ver;
+	int hor;
+
+	ver = 0;
+
+	for (hor = 0; hor != 8; hor++)
+	{
+		for (ver = 0; ver != 8; ver++)
+		{
+			_putchar(a[hor][ver]);
+		}
+		_putchar('\n');
+	}
+}
+
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - sums diagonals of a square
+ * @a: the square of integers
+ * @size: the size of the square
+ * Return: Always 0.
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, sum1, sum2;
+
+	sum1 = 0;
+	sum2 = 0;
+	for (i = 0; i < size * size; i += size + 1)
+		sum1 += a[i];
+	for (i = size - 1; i < size * size - 1; i += size - 1)
+		sum2 += a[i];
+	printf("%d, %d\n", sum1, sum2);
+
+
+}
+
+
